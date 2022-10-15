7c82f629fabc889e1bf110349d732578df1850b0
unknown <mo7amed.maki@gmail.com>
2022-10-15 17:39:30 +0200

    Implement updater the correct way

--- anony-flash-wp.php
+++ anony-flash-wp.php
@@ -30,24 +30,11 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+define( 'PLUGIN_SLUG', plugin_basename(__FILE__) );
+
 include_once('updater.php');
 
-if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
-    $config = array(
-        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-        'proper_folder_name' => 'anony-flash-wp', // this is the name of the folder your plugin lives in
-        'api_url' => 'https://api.github.com/repos/MakiOmar/AnonyEngine-Flash-WP', // the GitHub API url of your GitHub repo
-        'raw_url' => 'https://raw.github.com/MakiOmar/AnonyEngine-Flash-WP/master', // the GitHub raw url of your GitHub repo
-        'github_url' => 'https://github.com/MakiOmar/AnonyEngine-Flash-WP', // the GitHub url of your GitHub repo
-        'zip_url' => 'https://github.com/MakiOmar/AnonyEngine-Flash-WP/zipball/master', // the zip url of the GitHub repo
-        'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
-        'requires' => '3.0', // which version of WordPress does your plugin require?
-        'tested' => '3.3', // which version of WordPress is your plugin tested up to?
-        'readme' => 'README.md', // which file to use as the readme for the version number
-        'access_token' => '', // Access private repositories by authorizing under Plugins > GitHub Updates when this example plugin is installed
-    );
-    new WP_GitHub_Updater($config);
-}
+
 /**
  * Activation hook
  */ 
