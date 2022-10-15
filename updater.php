a44adc0f3dafa0ddd3464e729d6278fdd7e5bb67
unknown <mo7amed.maki@gmail.com>
2022-10-15 12:38:46 +0200

    upply WordPress GitHub Plugin Updater

--- updater.php
+++ updater.php
@@ -1,5 +1,4 @@
 <?php
-
 // Prevent loading this file directly and/or if the class is already defined
 if ( ! defined( 'ABSPATH' ) || class_exists( 'WPGitHubUpdater' ) || class_exists( 'WP_GitHub_Updater' ) )
 	return;
