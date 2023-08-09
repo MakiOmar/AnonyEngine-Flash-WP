ee69fc316e4f3b9ca40e5be456efaeeeda0134eb
makiomar <mo7amed.maki@gmail.com>
2023-08-09 12:01:21 +0300

    Apply to be injected styles

--- includes/class-anony-flash-wp.php
+++ includes/class-anony-flash-wp.php
@@ -206,6 +206,7 @@ class Anony_Flash_Wp {
 
 		$this->loader->add_filter( 'style_loader_tag', $plugin_public, 'remove_all_stylesheets', 99 );
 		$this->loader->add_filter( 'style_loader_tag', $plugin_public, 'stylesheet_media_to_print', 99 );
+		$this->loader->add_filter( 'style_loader_tag', $plugin_public, 'to_be_injected_styles', 99 );
 		$this->loader->add_filter( 'wp_footer', $plugin_public, 'load_stylesheets_upon_interact', 99 );
 
 		$this->loader->add_action( 'get_header', $plugin_public, 'wp_html_compression_finish' );
