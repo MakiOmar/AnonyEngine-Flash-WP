28368726fa2896aefaa7440a1e302810b28c935f
makiomar <mo7amed.maki@gmail.com>
2023-12-12 13:21:08 +0200

    Ability to dequeue styles for a single post/page

--- public/class-anony-flash-wp-public.php
+++ public/class-anony-flash-wp-public.php
@@ -1104,24 +1104,32 @@ class Anony_Flash_Wp_Public {
 		}
 		return $content;
 	}
-
+	/**
+	 * Dequeue unwanted styles
+	 *
+	 * @return void
+	 */
 	public function dequeued_styles() {
-		if ( is_admin() || $this->uri_strpos( 'elementor' ) ) {
+		if ( is_admin() || $this->uri_strpos( 'elementor' ) || ! class_exists( 'ANONY_STRING_HELP' ) ) {
 			return;
 		}
 
-		if ( class_exists( 'ANONY_STRING_HELP' ) ) {
-			$anofl_options = ANONY_Options_Model::get_instance( 'Anofl_Options' );
+		$anofl_options   = ANONY_Options_Model::get_instance( 'Anofl_Options' );
+		$dequeued_styles = ANONY_STRING_HELP::line_by_line_textarea( $anofl_options->dequeued_styles );
 
-			$dequeued_styles = ANONY_STRING_HELP::line_by_line_textarea( $anofl_options->dequeued_styles );
+		if ( is_singular() ) {
+			global $post;
+			$optimize_per_post    = get_post_meta( $post->ID, 'optimize_per_post', true );
+			$post_dequeued_styles = ANONY_STRING_HELP::line_by_line_textarea( $optimize_per_post['dequeued_styles'] );
+			$dequeued_styles      = array_merge( $dequeued_styles, $post_dequeued_styles );
+		}
 
-			if ( ! empty( $dequeued_styles ) ) {
-				foreach ( $dequeued_styles as $handle ) {
+		if ( is_array( $dequeued_styles ) ) {
+			foreach ( $dequeued_styles as $handle ) {
 
-					wp_dequeue_style( $handle );
-					wp_deregister_style( $handle );
+				wp_dequeue_style( $handle );
+				wp_deregister_style( $handle );
 
-				}
 			}
 		}
 	}
