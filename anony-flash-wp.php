962ddc597bb5159e60e47e413085b27311889330
makiomar <mo7amed.maki@gmail.com>
2024-06-25 18:27:40 +0300

    version update

--- anony-flash-wp.php
+++ anony-flash-wp.php
@@ -15,7 +15,7 @@
  * Plugin Name:       AnonyEngine Flash WP
  * Plugin URI:        http://makiomar.com
  * Description:       For WordPress higher speed.
- * Version:           1.0.109
+ * Version:           1.0.110
  * Author:            Makiomar
  * Author URI:        https://github.com/MakiOmar
  * License:           GPL-2.0+
